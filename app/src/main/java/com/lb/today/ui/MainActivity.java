bf2a1391441538712a0c1471bc1951fb163fc693
刘博 <liubo@6clue.com>
2016-01-17 00:08:40 +0800

    项目创建

---
+++ app/src/main/java/com/lb/today/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lb.today.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lb.today.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
